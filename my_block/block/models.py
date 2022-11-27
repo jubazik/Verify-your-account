90d209ccbe9b0cbe409a89b82e51b6e612d22cc3
zizik <hitgas12@mail.ru>
2022-11-27 14:53:10 +0300

    add model blog

--- my_block/block/models.py
+++ my_block/block/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Blog(models.Model):
+    title = models.CharField(max_length=60)
+    date = models.DateTimeField()
+    text = models.TextField
+    image = models.ImageField(upload_to='event_images/')
+
+
 # Create your models here.
