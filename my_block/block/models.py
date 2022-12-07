e3c65ea50df8c5fce5a646eaa5a4386c7a677a5b
jubazik <hitgas12@mail.ru>
2022-12-07 22:54:52 +0300

    add records

--- my_block/block/models.py
+++ my_block/block/models.py
@@ -8,4 +8,12 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='event_images/')
 
 
+
+class Records(models.Model):
+    data = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=30)
+    sum = models.IntegerField()
+    coment = models.CharField(max_length=80, blank=True)
+
+
 # Create your models here.
