b0082f737c40d4b4a143c29496db06ec07083026
jubazik <hitgas12@mail.ru>
2023-01-08 19:43:38 +0300

    new commit

--- my_block/block/urls.py
+++ my_block/block/urls.py
@@ -3,8 +3,9 @@ from .views import *
 
 
 urlpatterns = [
-    path('', counter, name='counterparties_block'),
+    path('', counter, name='counter'),
     path('records/', records, name='record'),
     # path('bl/', blog_all, name='blog'),
     path('records/<int:record_id>/', get_record_id, name='get_record_id'),
+    path('cunter/<int:coun_id>/', counterparties_category, name='count_id'),
 ]
--- my_block/block/views.py
+++ my_block/block/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Records, Сounterparties, Blog, Category
 
 
@@ -12,39 +11,51 @@ from .models import Records, Сounterparties, Blog, Category
 #     return render(request, template_name='block/blog.html', context=context)
 #
 
-def records(request):
+def records(request):  # Вывод всех оплат
     records = Records.objects.all()
     counterparties = Сounterparties.objects.all()
     context = {
         'records': records,
         'title': 'Список Оплат',
-        'counterparties' : counterparties
+        'counterparties': counterparties
 
     }
     return render(request, template_name='block/records.html', context=context)
 
 
-def get_record_id(request, record_id):
+def get_record_id(request, record_id):  # Вывод оплат по id
     record = Records.objects.filter(name_id=record_id)
     counterparties = Сounterparties.objects.all()
     counterpart = Сounterparties.objects.get(pk=record_id)
-    return render(request, 'block/record_id.html', {'record': record, 'counterparties': counterparties, 'caunterpart': counterpart })
+    return render(request, 'block/record_id.html',
+                  {'record': record, 'counterparties': counterparties, 'caunterpart': counterpart})
 
-def counter(request):
+
+def counter(request):  # Вывод всех конрагентов
     counter = Сounterparties.objects.all()
     context = {
         'counter': counter,
     }
     return render(request, template_name='block/count.html', context=context)
-def counterparties_category(request, coun_id):
+
+
+def counterparties_category(request, coun_id):  # Вывод контрагенты по id
     counterparties = Сounterparties.objects.filter(ctegory_id=coun_id)
     category = Category.objects.all()
     counter = Сounterparties.objects.get(pk=coun_id)
     context = {
         'counterparties': counterparties,
         'title': 'Список контрагентов',
-        'category' : category,
-        'counter' : counter
+        'category': category,
+        'counter': counter
     }
     return render(request, template_name='block/counterparties.html', context=context)
 # Create your views here.
+
+# def counter_id(request):
+#     category = Category.objects.all()
+#     context = {
+#         'title' : 'Список Контрагентов',
+#         'category' : category
+#     }
+#     return render(request, template_name='block/counterparties.html', context=context)
